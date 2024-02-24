e97595cbcbb622c53ccecbd0a565dabb077709c8
France020800 <francescomarchini7@gmail.com>
2024-02-24 17:10:49 +0100

    Added function to process single image

--- functions/functions.py
+++ functions/functions.py
@@ -10,6 +10,16 @@ def augment_images(images, transformations):
 
     return transformed_images
 
+def augment_image(image, transformations):
+    transformed_images = []
+    for transformation in transformations:
+        image_to_resize = transformation(image=image)['image']
+        resized_image = _image_resized(image_to_resize)
+        transformed_images.append(resized_image)
+
+    return transformed_images
+
+
 def _image_resized(image):
     height, width = image.shape[:2]
     if height > 1536 or width > 1536:
