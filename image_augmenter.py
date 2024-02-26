0c05871b3b3fbf4775037f027b283c9892b27deb
France020800 <francescomarchini7@gmail.com>
2024-02-26 12:37:53 +0100

    Create a single random transformation

--- image_augmenter.py
+++ image_augmenter.py
@@ -10,46 +10,27 @@ image_list = os.listdir(folder_path)
 images = [cv2.imread(folder_path + image) for image in image_list]
 
 # Transform objects
-brightnessTransform = A.Compose([
-    A.RandomBrightnessContrast(brightness_limit=0.7, contrast_limit = 0.7, p=1.0),
-])
-
-cropTransform = A.Compose([
-    A.RandomCrop(width=1536, height=1536, p=1.0),
-])
-
-rotateTransform = A.Compose([
-    A.Rotate(limit=45, p=1.0),
+transformation = A.Compose([
+    A.Rotate(limit=360, p=1.0),
+    A.VerticalFlip(p=0.5),
+    A.HorizontalFlip(p=0.5),
+    A.RandomBrightnessContrast(brightness_limit=0.7, contrast_limit = 0.7, p=0.5),
     A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
-])
-
-colorTransform = A.Compose([
-    A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
-])
-
-horizontalFlipTransform = A.Compose([
-    A.HorizontalFlip(p=1.0),
-    A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
-])
-
-verticalFlipTransform = A.Compose([
-    A.VerticalFlip(p=1.0),
-    A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
+    A.RandomCrop(width=1536, height=1536, p=0.2)
 ])
 
 
-transformations = [brightnessTransform, cropTransform, rotateTransform, colorTransform, horizontalFlipTransform, verticalFlipTransform]
 print('Augmenting images...')
 start_time = time.time()
-transformed_images = augment_images(images, transformations)
+transformed_images = augment_images(images, transformation)
 end_time = time.time() 
 print(f'Time taken to augment {len(images)} images: {round(end_time - start_time, 4)} seconds')
 
-print('Saving images...')
-index = 0
-for image in transformed_images:
-    out_path = f'out_images/augmented_demo_kitty_{index}.jpg'
-    cv2.imwrite(out_path, image)
-    index += 1
+# print('Saving images...')
+# index = 0
+# for image in transformed_images:
+#     out_path = f'out_images/augmented_demo_kitty_{index}.jpg'
+#     cv2.imwrite(out_path, image)
+#     index += 1
 print('Done!')
 
--- parallel_image_augmenter.py
+++ parallel_image_augmenter.py
@@ -2,39 +2,21 @@ import os
 import sys
 import cv2
 import time
-import albumentations as A
 import multiprocessing
+import albumentations as A
 from functions.functions import augment_images
-from multiprocessing import Pool
+from joblib import Parallel, delayed
 
 if __name__ == '__main__':
 
     # Transform objects
-    brightnessTransform = A.Compose([
-        A.RandomBrightnessContrast(brightness_limit=0.7, contrast_limit = 0.7, p=1.0),
-    ])
-
-    cropTransform = A.Compose([
-        A.RandomCrop(width=1536, height=1536, p=1.0),
-    ])
-
-    rotateTransform = A.Compose([
-        A.Rotate(limit=45, p=1.0),
+    transformation = A.Compose([
+        A.Rotate(limit=360, p=1.0),
+        A.VerticalFlip(p=0.5),
+        A.HorizontalFlip(p=0.5),
+        A.RandomBrightnessContrast(brightness_limit=0.7, contrast_limit = 0.7, p=0.5),
         A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
-    ])
-
-    colorTransform = A.Compose([
-        A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
-    ])
-
-    horizontalFlipTransform = A.Compose([
-        A.HorizontalFlip(p=1.0),
-        A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
-    ])
-
-    verticalFlipTransform = A.Compose([
-        A.VerticalFlip(p=1.0),
-        A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.6),
+        A.RandomCrop(width=1536, height=1536, p=0.2)
     ])
 
     # Load images
@@ -44,35 +26,31 @@ if __name__ == '__main__':
 
     # Create a pool of workers 
     if len(sys.argv) > 1:
-        pool_size = int(sys.argv[1])
+        num_thread = int(sys.argv[1])
     else:
-        pool_size = multiprocessing.cpu_count()
-    pool = Pool(processes=pool_size)
-    # print('Using {} processes'.format(pool_size))
+        num_thread = multiprocessing.cpu_count()
+    print('Using {} processes'.format(num_thread))
+    
+    
+    # Split images into batches
+    batch_size = round(len(images) // num_thread)
+    image_batches = [images[i:i+batch_size] for i in range(0, len(images), batch_size)]
 
     # Split images into batches
-    batch_size = round(len(images) // pool_size)
-    image_batches = [images[i:i+batch_size] for i in range(0, len(images), batch_size)]
-    args = [(batch, [brightnessTransform, cropTransform, rotateTransform, colorTransform, horizontalFlipTransform, verticalFlipTransform]) for batch in image_batches]
-
-    # Augment images
-    # print('Augmenting images...')
     start_time = time.time()
-    results = pool.starmap(augment_images, args)
-    pool.close()
-    pool.join()
+    transformed_images = Parallel(n_jobs=num_thread)(delayed(augment_images)(batch, transformation) for batch in image_batches)
     end_time = time.time()
+    print(f'{round(end_time - start_time, 4)}')
     # print(f'Time taken to augment {len(images)} images: {end_time - start_time} seconds')
 
     # Flatten the list of lists
-    transformed_images = [image for sublist in results for image in sublist]
+    # transformed_images = [image for sublist in results for image in sublist]
 
-
+    # print(f'Length of transformed images: {len(transformed_images[0])}')
     # print('Saving images...')
-    index = 0
-    for image in transformed_images:
-        out_path = f'out_images/augmented_demo_kitty_{index}.jpg'
-        cv2.imwrite(out_path, image)
-        index += 1
+    # index = 0
+    # for image in transformed_images:
+    #     out_path = f'out_images/augmented_demo_kitty_{index}.jpg'
+    #     cv2.imwrite(out_path, image)
+    #     index += 1
     print('Done!')
-    print(f'{round(end_time - start_time, 4)}')
